70902cc02fba9ba676740f4c221fc954d7d310af
cloudone.knr <cloudone.knr@gmail.com>
2023-03-19 14:02:49 -0400

    new ec2

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -5,11 +5,13 @@ terraform {
       version = "~>3.0"
     }
   }
+  backend "s3" {
+    bucket = "nr-terraform-state-bucket"
+    key    = "projects/labs/aws-labs/ec2"
+    region = "us-east-1"
+    }
 }
 
 provider "aws" {
   region = "us-east-1"
-  assume_role {
-    role_arn = "arn:aws:iam::600605052115:role/devops"
-  }
 }
