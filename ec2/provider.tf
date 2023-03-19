ba89dc790ec8f135bc97af514fcf83df8ef194e8
cloudone.knr <cloudone.knr@gmail.com>
2023-03-19 16:05:32 -0400

    modify jenkins

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -5,11 +5,12 @@ terraform {
       version = "~>3.0"
     }
   }
-  # backend "s3" {
-  #   bucket = "nr-terraform-state-bucket"
-  #   key    = "projects/labs/aws-labs/ec2"
-  #   region = "us-east-1"
-  #   }
+  backend "s3" {
+    bucket = "nr-terraform-state-bucket"
+    key    = "projects/labs/aws-labs/ec2"
+    region = "us-east-1"
+    role_arn = "arn:aws:iam::600605052115:role/devops"
+    }
 }
 
 provider "aws" {
