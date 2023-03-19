36a8e05358c5ac4456ec88aea855e8ad53139bc4
cloudone.knr <cloudone.knr@gmail.com>
2023-03-19 15:20:15 -0400

    add jenkins file

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -5,13 +5,16 @@ terraform {
       version = "~>3.0"
     }
   }
-  backend "s3" {
-    bucket = "nr-terraform-state-bucket"
-    key    = "projects/labs/aws-labs/ec2"
-    region = "us-east-1"
-    }
+  # backend "s3" {
+  #   bucket = "nr-terraform-state-bucket"
+  #   key    = "projects/labs/aws-labs/ec2"
+  #   region = "us-east-1"
+  #   }
 }
 
 provider "aws" {
   region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::600605052115:role/devops"
+  }
 }
